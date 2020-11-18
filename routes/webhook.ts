62355b1253232ffc0b606ad0c3a20c6eaf0ce4b5
Richard Robinson <rir@my.yorku.ca>
2020-11-18 03:04:09 -0500

    added logging; fixed webhook bug

--- routes/webhook.ts
+++ routes/webhook.ts
@@ -16,11 +16,12 @@ router.post('/', async (req, res) => {
     console.log(`webhook data received: ${JSON.stringify(req.body)}\n\n`)
 
     if (!validEvents.some(e => Object.keys(req.body).includes(e))) {
+        console.log('webhook event not valid')
         res.sendStatus(200)
         return
     }
 
-    if (req.body['installation']) {
+    if (req.body['installation'] && req.body['installation']['account']) {
         if (req.body['action'] === 'created') {
             await Installation.create({
                 installationId: req.body['installation']['id'],
@@ -36,6 +37,8 @@ router.post('/', async (req, res) => {
         .findOne({ installationId: req.body['installation']['id'] })
         .map(installation => installation.githubId)
 
+    console.log(`github id is ${githubId}`)
+
     if (!githubId) {
         res.status(500).send(`Installation with id ${req.body['installation']['id']} not found.`)
     }
@@ -43,6 +46,8 @@ router.post('/', async (req, res) => {
     const deviceToken = await User
         .findOne({ githubId: githubId })
         .map(user => user.deviceToken)
+
+    console.log(`device token is ${deviceToken}`)
 
     if (!deviceToken) {
         res.status(500).send(`User with github id ${githubId} not found`)
