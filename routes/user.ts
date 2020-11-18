4bb0e4e4304a26afad05e1c1500fa0e8e537ec6a
Richard Robinson <rir@my.yorku.ca>
2020-11-18 02:39:44 -0500

    added error handling to `/users`

--- routes/user.ts
+++ routes/user.ts
@@ -4,8 +4,13 @@ import {User} from "../models/user";
 export const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
-    console.log(`User created: ${JSON.stringify(user)}`)
+    try {
+        const user = await User.create(req.body)
+        console.log(`User created: ${JSON.stringify(user)}`)
 
-    res.sendStatus(200)
+        res.sendStatus(200)
+    } catch (e) {
+        console.log(`Failed to create user: ${JSON.stringify(req.body)}`)
+        res.sendStatus(500);
+    }
 })
