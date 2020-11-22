a4dc9a1834ad0e23b1e0abf8b87d7d0a56d0cc61
Richard Robinson <rir@my.yorku.ca>
2020-11-22 01:35:34 -0500

    fixed user schema issue

--- models/user.ts
+++ models/user.ts
@@ -5,14 +5,14 @@ export interface IUser {
     accessToken: string
     githubId: number
     deviceToken: string
-    latestEvent: Event
+    latestEvent?: Event
 }
 
 const UserSchema = new Schema({
     accessToken: { type: String, required: true },
     githubId: { type: Number, required: true },
     deviceToken: { type: String, required: true },
-    latestEvent: { type: Event }
+    latestEvent: { type: Object }
 })
 
 export const User: Model<IUser & Document> = model('User', UserSchema);
